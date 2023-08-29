76e61e8a9abfc28b331016524b630b7215732de9
CodyTave <dahiroussama@gmail.com>
2023-08-29 16:30:52 +0100

    23/08

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,12 @@ export default {
       colors: {
         primary: ["#009fe3"],
         secondary: ["#2b2c6b", "#916dde", "#ebebff", "#6163b4", "#23245d"],
-        dark: [""],
         light: ["#ebebff"],
         ph: ["#ababc4", "#e3e3ec", "#f4f4f4", "#636496"],
       },
       backgroundColor: {
         primary: ["#009fe3"],
         secondary: ["#2b2c6b", "#916dde", "#ebebff", "#6163b4", "#23245d"],
-        dark: [""],
         light: ["#ebebff"],
         ph: ["#ababc4", "#e3e3ec", "#f4f4f4", "#636496"],
       },
